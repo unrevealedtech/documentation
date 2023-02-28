a88bac457f74f6a4eb474736ed33491737350a7f
Thomas Groutars <tgroutars@gmail.com>
2023-02-28 11:44:56 +0100

    add social card and edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,18 +10,12 @@ const config = {
   tagline: 'Workflow based feature management',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://docs.unrevealed.tech',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -34,10 +28,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // // Please change this to your repo.
-          // // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: 'https://github.com/unrevealedtech/documentation/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -49,8 +40,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      // image: 'img/docusaurus-social-card.jpg',
+      image: 'img/unrevealed-social-card.png',
       navbar: {
         title: 'Unrevealed',
         logo: {
